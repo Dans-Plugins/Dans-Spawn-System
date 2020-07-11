0aae1ae152455b458c2f7c2ad3ea64dd2f588125
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-11 14:40:16 -0600

    initial commit:

---
+++ src/main/java/kingdomprogrammers/kingdomdarkagesspawn/KingdomDarkAgesSpawn.java
@@ -0,0 +1,17 @@
+package kingdomprogrammers.kingdomdarkagesspawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KingdomDarkAgesSpawn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
