fa8d98ea6a12550104c17e1725170bb6ab5f0a2f
dmccoystephenson <dmccoystephenson@gmail.com>
2020-08-12 12:14:34 -0600

    disallowed breaking blocks next to spawn selection signs

--- src/main/java/spawnsystem/EventHandlers/BlockBreakEventHandler.java
+++ src/main/java/spawnsystem/EventHandlers/BlockBreakEventHandler.java
@@ -1,9 +1,12 @@
 package spawnsystem.EventHandlers;
 
 import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.event.block.BlockBreakEvent;
 import spawnsystem.Main;
+
+import java.util.ArrayList;
 
 public class BlockBreakEventHandler {
 
@@ -30,6 +33,41 @@ public class BlockBreakEventHandler {
             }
 
         }
+        else {
+            if (isNextToSpawnSelectionSign(event.getBlock())) {
+
+                if (event.getPlayer().hasPermission("spawnsystem.breakSpawnSign") || event.getPlayer().hasPermission("spawnsystem.admin")) {
+                    event.getPlayer().sendMessage(ChatColor.GREEN + "Spawn selection sign broken!");
+                }
+                else {
+                    event.getPlayer().sendMessage(ChatColor.RED + "Sorry! In order to break a spawn selection sign, you must have the following permission: 'spawnsystem.breakSpawnSign");
+                    event.setCancelled(true);
+                }
+
+            }
+        }
+    }
+
+    private boolean isNextToSpawnSelectionSign(Block block) {
+        // declare blocks
+        ArrayList<Block> blocks = new ArrayList<>();
+        blocks.add(block.getWorld().getBlockAt(block.getX(), block.getY() + 1, block.getZ()));
+        blocks.add(block.getWorld().getBlockAt(block.getX(), block.getY() - 1, block.getZ()));
+        blocks.add(block.getWorld().getBlockAt(block.getX() + 1, block.getY(), block.getZ()));
+        blocks.add(block.getWorld().getBlockAt(block.getX() - 1, block.getY(), block.getZ()));
+        blocks.add(block.getWorld().getBlockAt(block.getX(), block.getY(), block.getZ() + 1));
+        blocks.add(block.getWorld().getBlockAt(block.getX(), block.getY(), block.getZ() - 1));
+
+        for (Block b : blocks) {
+            if (main.utilities.isSign(b)) {
+                Sign sign = (Sign) b.getState();
+                if (sign.getLine(0).contains("[Spawn]")) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
 }
