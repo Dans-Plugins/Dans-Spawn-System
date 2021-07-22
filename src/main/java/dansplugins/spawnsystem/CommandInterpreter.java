c26fa99ce32cccb4c25ffdab370ca1b478727fd3
BuildTools <unconfigured@null.spigotmc.org>
2021-07-22 17:54:24 -0600

    Abstracted out the Reset Spawn command into its own command class

--- src/main/java/dansplugins/spawnsystem/CommandInterpreter.java
+++ src/main/java/dansplugins/spawnsystem/CommandInterpreter.java
@@ -1,4 +1,5 @@
 package dansplugins.spawnsystem;
+import dansplugins.spawnsystem.commands.ResetSpawnCommand;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -8,40 +9,9 @@ public class CommandInterpreter {
     public boolean interpretCommand(CommandSender sender, String label, String[] args) {
 
         if (label.equalsIgnoreCase("resetspawn")) {
-
-            // if sender instanceof player
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-
-                if (args.length > 0) {
-                    if (player.hasPermission("dansplugins.spawnsystem.reset.others") || player.hasPermission("dansplugins.spawnsystem.admin")) {
-                        // reset a specific player's spawn
-                        String targetPlayer = args[0];
-                        UtilitySubsystem.getInstance().resetSpawn(UtilitySubsystem.findUUIDBasedOnPlayerName(targetPlayer));
-                        player.sendMessage(ChatColor.GREEN + "Spawn reset for " + targetPlayer + "!");
-                        try {
-                            DansSpawnSystem.getInstance().getServer().getPlayer(targetPlayer).sendMessage(ChatColor.GREEN + "Your spawn has been reset!");
-                        } catch(Exception e) {
-
-                        }
-                    }
-                    else {
-                        player.sendMessage(ChatColor.RED + "Sorry! In order to use this command, you need the following permission: 'dansplugins.spawnsystem.reset.others'");
-                    }
-
-                }
-                else {
-                    if (player.hasPermission("dansplugins.spawnsystem.reset.self") || player.hasPermission("dansplugins.spawnsystem.admin")) {
-                        // reset the spawn of the command sender
-                        UtilitySubsystem.getInstance().resetSpawn(UtilitySubsystem.findUUIDBasedOnPlayerName(player.getName()));
-                        player.sendMessage(ChatColor.GREEN + "You have reset your spawn!");
-                    }
-                    else {
-                        player.sendMessage(ChatColor.RED + "Sorry! In order to use this command, you need the following permission: 'dansplugins.spawnsystem.reset.self'");
-                    }
-                }
-            }
-
+            ResetSpawnCommand command = new ResetSpawnCommand();
+            command.execute(sender, args);
+            return true;
         }
 
         return false;
---
+++ src/main/java/dansplugins/spawnsystem/commands/ResetSpawnCommand.java
@@ -0,0 +1,46 @@
+package dansplugins.spawnsystem.commands;
+
+import dansplugins.spawnsystem.DansSpawnSystem;
+import dansplugins.spawnsystem.UtilitySubsystem;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ResetSpawnCommand {
+
+    public void execute(CommandSender sender, String[] args) {
+        // if sender instanceof player
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            if (args.length > 0) {
+                if (player.hasPermission("dansplugins.spawnsystem.reset.others") || player.hasPermission("dansplugins.spawnsystem.admin")) {
+                    // reset a specific player's spawn
+                    String targetPlayer = args[0];
+                    UtilitySubsystem.getInstance().resetSpawn(UtilitySubsystem.findUUIDBasedOnPlayerName(targetPlayer));
+                    player.sendMessage(ChatColor.GREEN + "Spawn reset for " + targetPlayer + "!");
+                    try {
+                        DansSpawnSystem.getInstance().getServer().getPlayer(targetPlayer).sendMessage(ChatColor.GREEN + "Your spawn has been reset!");
+                    } catch(Exception e) {
+
+                    }
+                }
+                else {
+                    player.sendMessage(ChatColor.RED + "Sorry! In order to use this command, you need the following permission: 'dansplugins.spawnsystem.reset.others'");
+                }
+
+            }
+            else {
+                if (player.hasPermission("dansplugins.spawnsystem.reset.self") || player.hasPermission("dansplugins.spawnsystem.admin")) {
+                    // reset the spawn of the command sender
+                    UtilitySubsystem.getInstance().resetSpawn(UtilitySubsystem.findUUIDBasedOnPlayerName(player.getName()));
+                    player.sendMessage(ChatColor.GREEN + "You have reset your spawn!");
+                }
+                else {
+                    player.sendMessage(ChatColor.RED + "Sorry! In order to use this command, you need the following permission: 'dansplugins.spawnsystem.reset.self'");
+                }
+            }
+        }
+    }
+
+}
